c50d0f4b217e4312c573b039dd6d328692f0237b
Konstantin Kobs <mail@konstantinkobs.de>
2016-11-02 10:50:30 +0100

    optimized a bit and added plot output

--- Exercise 2/logisticRegression.m
+++ Exercise 2/logisticRegression.m
@@ -1,6 +1,6 @@
 function theta = logisticRegression(data, alpha)
 
-    theta = rand([3,1]) * 0.2 - 0.1
+    theta = rand([3,1]) * 0.02 - 0.01;
     [rows, columns] = size(data);
     vec = ones([rows, 1]);
     
@@ -15,20 +15,21 @@ function theta = logisticRegression(data, alpha)
     while(errorDecrease / alpha > threshold)
     
         % compute matrix product of X and Theta 
-        % After that we can compute the result of the logistic funtion
+        % After that we can compute the result of the logistic function
         intermediate = x_mat * theta;
-        h = arrayfun(@logsig, intermediate);
-        error = sum((y - h).^2);  
-        update = alpha * (y - h)'*x_mat;
+        h = logsig(intermediate); % logsig is the sigmoid function
+        diff = (y - h);
+        error = diff' * diff;
+        update = alpha * diff' * x_mat;
         theta = theta + update';
         
         if iteration > 0
             errorDecrease = ((errors(end) - error)/errors(end));
-        end   
+        end
         iteration = iteration + 1;
         errors = [errors error];
     end
-   
-
+    
+    display(['Converged after ' num2str(iteration) ' iterations.'])
 
 end
--- Exercise 2/readData.m
+++ Exercise 2/readData.m
@@ -1,15 +1,14 @@
 data = dlmread('data.txt',' ');
 
-alpha = 0.005;
+alpha = 0.05;
 
 theta = logisticRegression(data, alpha);
 
 positive = data(find(data(:,3) == 1),:);
 negative = data(find(data(:,3) == 0),:);
 figure
-scatter(positive(:,1),positive(:,2)); hold on;
-scatter(negative(:,1),negative(:,2)); hold on;
+scatter(positive(:,1), positive(:,2), [], 'green'); hold on;
+scatter(negative(:,1), negative(:,2), [], 'red'); hold on;
 display(theta);
 
 fplot(@(x) -1*(theta(1)+ theta(2)*x)/theta(3), [-3 3]); hold off;
-
